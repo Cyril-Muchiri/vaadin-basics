5a010ac972cb2b291dcbdb9895d7373bcf12101f
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2024-01-26 21:54:01 +0300

    styled some css

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -9,7 +9,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
-@Route("my-app")
+@Route("")
 public class MainView extends VerticalLayout {
 
     public MainView() {
